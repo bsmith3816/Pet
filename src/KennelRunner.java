55dfb81e163400ea993572b7c9f3a48c919f52b8
BT_1N3_29 <zchen5143@bths.edu>
2019-04-18 10:18:43 -0400

    a

--- src/KennelRunner.java
+++ src/KennelRunner.java
@@ -2,7 +2,7 @@ public class KennelRunner {
     public static void main(String[] args){
         Cat c = new Cat("bob1");
         Dog d = new Dog("bob2");
-        LoudDog ld = new LoudDog("bob3");;
+        LoudDog ld = new LoudDog("bob3"); 
         Pet[] petArray = new Pet[3];
         petArray[0] = c;
         petArray[1] = d;
